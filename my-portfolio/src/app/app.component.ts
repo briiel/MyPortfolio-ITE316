c19f86b7082fcaf4d75b60f514ab92285ba332ec
Syrean <144537019+briiel@users.noreply.github.com>
2024-08-13 13:34:05 +0800

    Part 1 Portfolio DONE
    completed the 1st part of my portfolio

--- my-portfolio/src/app/app.component.ts
+++ my-portfolio/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+declare const Typed: any;
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -10,4 +12,15 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'my-portfolio';
+
+  ngAfterViewInit (){
+    const typed = new Typed ('.multiple-text', {
+      strings: ["Frontend Developer", "Graphic Designer", "Video Editor"],
+      typeSpeed: 70,
+      backSpeed: 70,
+      backDelay: 1000,
+      loop: true,
+    });
+  }
+
 }
